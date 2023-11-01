55aa32937aa5be37a3b069f87a1aab1cb492bd35
Justin "J.R." Hill <justin@so.dang.cool>
2023-11-01 14:35:03 -0700

    make it executable

--- bench.py
+++ bench.py
@@ -1,8 +1,22 @@
+#!/usr/bin/env python
+
+import argparse
 import asyncio
 from datetime import datetime
 import openfga_sdk
 import openfga_sdk.sync
 import os
+
+
+parser = argparse.ArgumentParser(
+    prog='bench.py',
+    description='Benchmarks async vs sync batch_check performance',
+    epilog='Sources can be found at https://github.com/booniepepper/openfga-python-batch-check-bench/'
+)
+parser.add_argument('mode', choices=['async', 'sync'])
+parser.add_argument('-n', '--n-requests', default=5000)
+
+args = parser.parse_args()
 
 
 configuration = openfga_sdk.ClientConfiguration(
@@ -21,7 +35,7 @@ configuration = openfga_sdk.ClientConfiguration(
 
 
 requests = []
-for _ in range(500_000):
+for _ in range(args.n_requests):
     key = openfga_sdk.TupleKey()
     request = openfga_sdk.CheckRequest(key)
     requests.append(request)
@@ -43,11 +57,11 @@ def sync_batch_check():
     print("Sync elapsed seconds:", end - start)
 
 
-
 def main():
-    asyncio.run(async_batch_check())
-
-    sync_batch_check()
+    if (args.mode == 'async'):
+        asyncio.run(async_batch_check())
+    elif (args.mode == 'sync'):
+        sync_batch_check()
 
 
 if __name__ == '__main__':
